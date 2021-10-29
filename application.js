414f68e53c15e472cb6b6e7c19f7da5abee7be84
anghari <22214030+chesterlbtan@users.noreply.github.com>
2021-10-30 00:45:39 +0800

    Update the page with improved js

--- application.js
+++ application.js
@@ -1,34 +1,121 @@
+
 $(document).ready(function () {
-    console.log("testing");
+    var data_url = document.getElementById("data_url").innerHTML
+    console.log("prior ajax or json");
     $.ajax({
         type: "GET",
-        url: "trynow.csv",
-        dataType: "text",
-        success: function(response)
-        {
-        data = $.csv.toArrays(response);
-        displayHTMLTable(data);
+        url: data_url,
+        dataType: "json",
+        success: function(response) {
+            displayHTMLTable(response);
         }
-        })
-                    });
+    });
+    document.getElementById("gametab").click();
+});
 
-    function displayHTMLTable(results){
-	var table = "<table>";
-	var data = results.data;
-	console.log(data);
+function displayHTMLTable(data) {
+    // Process Team
+    console.log("Processing Team data");
+    processTeamTables(data["Team"]);
 
-	for(i=0;i<data.length;i++){
+    eve = new Event("update", {bubbles: true});
+    document.getElementById("team_avg_stats").dispatchEvent(eve);
+}
+
+function processTeamTables(team_data) {
+    // Process averages
+    var avg_data = team_data["average"];
+	var table = "<tbody>";
+	for(i=0;i<avg_data.length;i++){
 		table+= "<tr>";
-		var row = data[i];
-		var cells = row.join(",").split(",");
-
-		for(j=0;j<cells.length;j++){
-			table+= "<td>";
-			table+= cells[j];
-			table+= "</th>";
+		row = avg_data[i];
+		for(var j in row) {
+		    table += "<td>";
+		    if(j=="Logo"){
+			    table += "<img src=\"https://cdn.datdota.com/images/" + row[j] + ".png\" width=\"45\" height=\"27\">";
+		    }
+			else if(["WinRate", "Kills", "Deaths"].includes(j)){
+			    // table += String.format("%.2f", cells[j]);
+			    table += parseFloat(row[j]).toFixed(2);
+			}
+			else if(j=="Duration"){
+			    minutes = Math.floor(row[j] / 60);
+			    seconds = row[j] % 60;
+			    table += minutes + ":" + String(seconds.toFixed(0)).padStart(2, '0');
+			}
+			else{
+			    table+= row[j];
+			}
+			table+= "</td>";
 		}
 		table+= "</tr>";
 	}
-	table+= "</table>";
-	document.getElementById("parsed_csv_list").innerHTML = table;
+	table += "</tbody>";
+	temp = document.getElementById("team_avg_stats").innerHTML;
+	document.getElementById("team_avg_stats").innerHTML += table;
+
+    // Process Top Kills
+    var avg_data = team_data["kills"];
+	var table = "<tbody>";
+	for(i=0;i<avg_data.length;i++){
+		table+= "<tr>";
+		row = avg_data[i];
+		for(var j in row) {
+		    table += "<td>";
+		    if(j=="Logo"){
+			    table += "<img src=\"https://cdn.datdota.com/images/" + row[j] + ".png\" width=\"45\" height=\"27\">";
+		    }
+			else{
+			    table+= row[j];
+			}
+			table+= "</td>";
+		}
+		table+= "</tr>";
+	}
+	table += "</tbody>";
+	temp = document.getElementById("team_kills").innerHTML;
+	document.getElementById("team_kills").innerHTML += table;
+
+    // Process game duration
+    var avg_data = team_data["duration"];
+	var table = "<tbody>";
+	for(i=0;i<avg_data.length;i++){
+		table+= "<tr>";
+		row = avg_data[i];
+		for(var j in row) {
+		    table += "<td>";
+		    if(["Logo_R", "Logo_D"].includes(j)){
+			    table += "<img src=\"https://cdn.datdota.com/images/" + row[j] + ".png\" width=\"45\" height=\"27\">";
+		    }
+			else{
+			    table+= row[j];
+			}
+			table+= "</td>";
+		}
+		table+= "</tr>";
+	}
+	table += "</tbody>";
+	temp = document.getElementById("team_duration").innerHTML;
+	document.getElementById("team_duration").innerHTML += table;
+}
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
